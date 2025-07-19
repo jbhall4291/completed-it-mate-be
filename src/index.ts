3bed77a7fd28bdd8091f4491c754e7c46d2f442a
Johnny Hall <jbhall4291@gmail.com>
2025-07-19 17:07:28 +0100

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running! Hooray!!!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
