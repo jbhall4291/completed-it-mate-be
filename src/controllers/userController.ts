cc1723720c4e62b512d840692fa44e6ce48e35b2
Johnny Hall <jbhall4291@gmail.com>
2025-07-19 19:44:33 +0100

    add endpoint to create user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,3 +11,15 @@ export const getUsers = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error fetching users", error });
   }
 };
+
+
+export const createUser = async (req: Request, res: Response) => {
+  console.log('hit createUser!!')
+  try {
+    const { username, email } = req.body;
+    const newUser = await UserModel.create({ username, email, gamesOwned: [] });
+  res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: "Error creating user", error });
+  }
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,13 @@
 //userRoutes.ts
 import express from "express";
-import { getUsers } from "../controllers/userController";
+import { getUsers, createUser } from "../controllers/userController";
 
 const router = express.Router();
 
 // Route: GET /api/users
 router.get("/users", getUsers);
+
+// Route: POST /api/users
+router.post("/users", createUser);
 
 export default router;
