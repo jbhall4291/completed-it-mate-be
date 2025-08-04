f54e6d999ba474048bc364d86c38e70fce1a3d31
Johnny Hall <jbhall4291@gmail.com>
2025-08-04 13:15:44 +0100

    fix: update reset-library to use DELETE and add backfill test for Playwright teardown route

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -3,11 +3,20 @@ import { Request, Response } from 'express';
 import { UserModel } from "../models/User";
 
 export const resetTestUserLibrary = async (req: Request, res: Response) => {
-  const user = await UserModel.findOne({ username: 'test' });
-  if (!user) return res.status(404).send('Test user not found');
 
-  user.gamesOwned = [];
-  await user.save();
+  try {
+    const user = await UserModel.findOne({ username: 'test' });
+    if (!user) {
+      console.warn('Reset failed: test user not found');
+      return res.status(404).send('Test user not found');
+    }
 
-  res.sendStatus(204);
+    user.gamesOwned = [];
+    await user.save();
+
+    return res.sendStatus(204); // No Content
+  } catch (err) {
+    console.error('Error resetting test user library:', err);
+    return res.status(500).send('Internal server error');
+  }
 };
