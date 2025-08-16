df2f8ce8ca7f26daf57d681e34da32253b1caae7
Johnny Hall <jbhall4291@gmail.com>
2025-08-16 16:01:47 +0100

    backfill createUser tests and update userService

--- __tests__/createUser.test.ts
+++ __tests__/createUser.test.ts
@@ -1,34 +1,34 @@
-// addGameToUser.test.ts
+// createUser.test.ts
 import request from "supertest";
 import app from "../src/app";
 import { UserModel } from "../src/models/User";
 import mongoose from "mongoose";
 
-describe("POST /users", () => { 
+describe("POST /users", () => {
 
   const username = "johnny"
   const email = "johnny@gmail.com"
 
-    beforeEach(async () => {
+  beforeEach(async () => {
     await UserModel.deleteMany({});
   });
 
-  
-  it.only("adds a new user to the user collection", async () => {
-  const res = await request(app)
-    .post(`/api/users`)
-    .send({ username, email })
-    .expect(201);
 
-  expect(res.body.gamesOwned.length).toBe(0);
-  expect(res.body.username).toBe(username);
-  expect(res.body.email).toBe(email);
-  expect(res.body).toHaveProperty("_id");
+  it("adds a new user to the user collection", async () => {
+    const res = await request(app)
+      .post(`/api/users`)
+      .send({ username, email })
+      .expect(201);
 
-});
+    expect(res.body.gamesOwned.length).toBe(0);
+    expect(res.body.username).toBe(username);
+    expect(res.body.email).toBe(email);
+    expect(res.body).toHaveProperty("_id");
 
-it.only("returns 400 if user already exists with the given email", async () => {
-    
+  });
+
+  it("returns 400 if user already exists with the given email", async () => {
+
     await UserModel.create({ username, email });
 
     const res = await request(app)
@@ -39,74 +39,50 @@ it.only("returns 400 if user already exists with the given email", async () => {
     expect(res.body.message).toMatch("email already exists on an existing user");
   });
 
-// TODO:
-// check username doesn't already exist
-// check post request has expected body i.e. username and email 
-// validation on provided email, and/or in front end?
+
+  it("returns 400 if user already exists with the given username", async () => {
+
+    const differentEmail = "adifferentemail@email.com"
+    await UserModel.create({ username, email: differentEmail });
+
+    const res = await request(app)
+      .post("/api/users")
+      .send({ username, email })
+      .expect(400);
+
+    expect(res.body.message).toMatch("username already exists on an existing user");
+  });
 
 
-//   it("returns a 404 if the user ID doesn't exist", async () => {
-//       const fakeUserId = new mongoose.Types.ObjectId().toString();
+  it("falls back to 400 + generic message when service throws a plain Error ", async () => {
 
-//   const res = await request(app)
-//     .post(`/api/users/${fakeUserId}/games`)
-//     .send({ gameId })
-//     .expect(404);
+    const res = await request(app)
+      .post("/api/users")
+      .send({ email: "x@test.com" }) // no username
+      .expect(400);
 
-//     expect(res.body.message).toBe("User not found");
+    expect(res.body.message).toBe("Missing required fields");
+  });
 
-// });
+  it("returns 400 when email is missing", async () => {
+    const res = await request(app)
+      .post("/api/users")
+      .send({ username })  // no email
+      .expect(400);
 
-//   it("returns a 404 if the game ID doesn't exist", async () => {
-//     const fakeGameId = new mongoose.Types.ObjectId().toString();
+    expect(res.body.message).toBe("Missing required fields");
+  });
 
-//   const res = await request(app)
-//     .post(`/api/users/${userId}/games`)
-//     .send({ gameId: fakeGameId })
-//     .expect(404);
 
-//     expect(res.body.message).toBe("Game not found");
 
-// });
+  it("returns a 422 when the passed email fails basic validation check", async () => {
 
-//   it("returns a 400 if the user already owns the game", async () => {
+    const res = await request(app)
+      .post("/api/users")
+      .send({ username, email: "x@testcom" })
+      .expect(422);
 
-//     await request(app)
-//     .post(`/api/users/${userId}/games`)
-//     .send({ gameId })
-//     .expect(200);
-
-//     const res = await request(app)
-//       .post(`/api/users/${userId}/games`)
-//       .send({ gameId })
-//       .expect(400);
-
-//     expect(res.body.message).toBe("Game already owned");
-
-// });
-
-// it("returns a 400 if the game ID is invalid", async () => {
-
-  
-//     const res = await request(app)
-//       .post(`/api/users/${userId}/games`)
-//       .send({ gameId: "banana" })
-//       .expect(400);
-
-//     expect(res.body.message).toBe("Invalid game Id");
-
-// });
-
-// it("returns a 400 if the user ID is invalid", async () => {
-
-  
-//     const res = await request(app)
-//       .post(`/api/users/banana/games`)
-//       .send({ gameId })
-//       .expect(400);
-
-//     expect(res.body.message).toBe("Invalid user Id");
-
-// });
+    expect(res.body.message).toBe("Invalid email address");
+  });
 
 });
