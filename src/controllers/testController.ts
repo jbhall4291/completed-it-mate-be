435b396f4070f23756152c2c2e264475e6a8c07a
Johnny Hall <jbhall4291@gmail.com>
2025-08-26 11:57:45 +0100

    update debug route to clear test user's UserGame docs

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,22 +1,16 @@
 // controllers/testController.ts
 import { Request, Response } from 'express';
-import { UserModel } from "../models/User";
+import { UserModel } from '../models/User';
+import { UserGameModel } from '../models/UserGame';
 
 export const resetTestUserLibrary = async (req: Request, res: Response) => {
-
   try {
-    const user = await UserModel.findOne({ username: 'test' });
-    if (!user) {
-      // console.warn('Reset failed: test user not found');
-      return res.status(404).send('Test user not found');
-    }
-
-    user.gamesOwned = [];
-    await user.save();
+    // Find test user (hardcoded username or ID)
+    await UserGameModel.deleteMany({ userId: '6890a2561ffcdd030b19c08c' });
 
     return res.sendStatus(204); // No Content
   } catch (err) {
-    // console.error('Error resetting test user library:', err);
+    console.error('Error resetting test user library:', err);
     return res.status(500).send('Internal server error');
   }
 };
